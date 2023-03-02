ab5672499da65399d5aef4b3b39e24179bec25de
crichardd <crichard11@myges.fr>
2023-03-02 11:54:16 +0100

    mise en place des routes

---
+++ src/Login.tsx
@@ -0,0 +1,178 @@
+import React from 'react';
+import "./css/Login.css";
+
+
+
+export const showSignin = () => {
+    const prism = document.querySelector(".rec-prism") as HTMLElement;
+
+  if (prism) {
+      prism.style.transform = "translateZ(-100px)";
+  }
+}
+  
+export const showSignup = () => {
+    const prism = document.querySelector(".rec-prism") as HTMLElement;
+
+
+  if (prism) {
+    prism.style.transform = "translateZ(-100px) rotateY( -90deg)";
+  }
+}
+
+export const showForgotPassword = () => {
+
+    const prism = document.querySelector(".rec-prism") as HTMLElement;
+
+  if (prism) {
+    prism.style.transform = "translateZ(-100px) rotateY( -180deg)";
+  }
+}
+
+export const showSubscribe = () => {
+    const prism = document.querySelector(".rec-prism") as HTMLElement;
+
+  if (prism) {
+    prism.style.transform = "translateZ(-100px) rotateX( -90deg)";
+  }
+}
+
+export const showContactUs = () => {
+    const prism = document.querySelector(".rec-prism") as HTMLElement;
+
+  if (prism) {
+    prism.style.transform = "translateZ(-100px) rotateY( 90deg)";
+  }
+}
+
+export const showThankYou = () => {
+    const prism = document.querySelector(".rec-prism") as HTMLElement;
+
+  if (prism) {
+    prism.style.transform = "translateZ(-100px) rotateX( 90deg)";
+  }
+}
+
+export default function Login() {
+  return (
+    <div className="Login">
+      <header className="Login-header">
+      </header>
+      <body>
+        <ul className="nav">
+          <li onClick={showSignin}>Connexion</li>
+          <li onClick={showSignup}>Inscription</li>
+          <li onClick={showSubscribe}>News Letter</li>
+          <li onClick={showContactUs}>Contactez-nous</li>
+        </ul>
+        <div className="wrapper">
+          <div className="rec-prism">
+            <div className="face face-top">
+              <div className="content">
+              <h2>News Letter</h2>
+              <small>Entrez votre e-mail pour recevoir les dernières mises à jour !</small>
+              <form onSubmit={(event) => event.preventDefault()}>
+                <div className="field-wrapper">
+                  <input type="text" name="email" placeholder="e-mail"/>
+                  <label>e-mail</label>
+                </div>
+                <div className="field-wrapper">
+                  <input type="submit" onClick={showThankYou}/>
+                </div>
+                </form>
+              </div>
+            </div>
+            <div className="face face-front">
+              <div className="content">
+                <h2>Connexion</h2>
+                <form onSubmit={(event) => event.preventDefault()}>
+                  <div className="field-wrapper">
+                    <input type="text" name="username" placeholder="nom d'utilisateur"/>
+                    <label>nom d'utilisateur</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="password" name="password" placeholder="mot de passe" autoComplete="new-password"/>
+                    <label>mot de passe</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="submit" onClick={showThankYou}/>
+                  </div>
+                  <span className="psw" onClick={showForgotPassword}>Mot de passe oublié ? </span>
+                  <span className="signup" onClick={showSignup}>Pas encore de compte ? Inscrivez-vous</span>
+                </form>
+              </div>
+            </div>
+            <div className="face face-back">
+              <div className="content">
+                <h2>Mot de passe oublié ?</h2>
+                <small>Entrez votre e-mail pour recevoir un lien de réinitialisation de votre mot de passe</small>
+                <form onSubmit={(event) => event.preventDefault()}>
+                  <div className="field-wrapper">
+                    <input type="text" name="email" placeholder="e-mail"/>
+                    <label>e-mail</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="submit" onClick={showThankYou}/>
+                  </div>
+              </form>
+              </div>
+            </div>
+            <div className="face face-right">
+              <div className="content">
+                <h2>Inscription</h2>
+                <form onSubmit={(event) => event.preventDefault()}>
+                  <div className="field-wrapper">
+                    <input type="text" name="email" placeholder="email"/>
+                    <label>E-mail</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="password" name="password" placeholder="password" autoComplete="new-password"/>
+                    <label>Mot de passe</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="password" name="password2" placeholder="password" autoComplete="new-password"/>
+                    <label>Entrer à nouveau votre mot de passe</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="submit" onClick={showThankYou}/>
+                  </div>
+                  <span className="singin" onClick={showSignin}>Déjà utilisateur ? Connexion</span>
+                </form>
+              </div>
+            </div>
+            <div className="face face-left">
+              <div className="content">
+                <h2>Contactez-nous</h2>
+                <form onSubmit={(event) => event.preventDefault()}>
+                  <div className="field-wrapper">
+                    <input type="text" name="name" placeholder="name"/>
+                    <label>Nom</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="text" name="email" placeholder="email"/>
+                    <label>E-mail</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <textarea placeholder="your message"></textarea>
+                    <label>Votre message</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="submit" onClick={showThankYou}/>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="face face-bottom">
+              <div className="content">
+                <div className="thank-you-msg">
+                  Thank you!
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
+    </div>
+  );
+}
+
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './Login';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/connect" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// <App />
 reportWebVitals();
