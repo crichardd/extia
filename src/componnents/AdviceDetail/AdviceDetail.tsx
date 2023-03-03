00282e7893d23a2b6dbf879f6055dd7e03403fed
matthieudupas <matthieudupas@outlook.fr>
2023-03-03 11:12:02 +0100

    Advices page

--- src/componnents/AdviceDetail/AdviceDetail.tsx
+++ src/componnents/AdviceDetail/AdviceDetail.tsx
@@ -1,21 +1,44 @@
-import React from "react";
-interface AdviceDetail {
-    date:string;
-    message: string;
-    note: number;
+import React, {useEffect, useState} from "react";
+import "../../css/Advices.css"
+interface AdviceData {
+    dateOfComment: string;
+    content: string;
+    mark: number;
 }
 
-const AdviceCard = ({ date, message, note}: AdviceDetail) => {
+const AdviceCard = ({dateOfComment, mark, content}: AdviceData) => {
     return (
         <div className="advice-card">
-            <div className="advice-header-container">
-                <p>Note : {note}/5</p>
-                <p>{date}</p>
+            <div className="advice-card-header">
+                <p>Note : {mark}/5</p>
+                <p>{dateOfComment}</p>
             </div>
-            <p>{message}</p>
-
+            <div className="advice-card-body">
+                <p>{content}</p>
+            </div>
         </div>
     );
 };
 
-export default AdviceCard;
+const AdviceList : React.FC = () => {
+    const [advices, setAdvices] = useState<AdviceData[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('http://63.33.61.128:3000/api/comment/all');
+            const data = await response.json();
+            setAdvices(data);
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div className="advices-grid">
+            {advices.map((advice) => (
+                <AdviceCard dateOfComment={advice.dateOfComment.substring(0,10)} mark={advice.mark} content={advice.content}/>
+            ))}
+        </div>
+    );
+};
+
+export default AdviceList;
---
+++ src/componnents/Company/Company.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+
+import "../../css/company.css"
+
+interface CompanyData {
+    mark: string;
+    name: string;
+    logo: string;
+}
+
+const CompanyList : React.FC = () => {
+    const [companies, setCompanies] = useState<CompanyData[]>([]);
+    const [selectedItem, setSelectedItem] = useState<CompanyData>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('https://b352-94-228-190-38.eu.ngrok.io/api/company/all');
+            const data = await response.json();
+            setCompanies(data);
+        };
+        fetchData();
+    }, []);
+
+    /*
+    const filterList = (name: string) =>  {
+        companies.forEach((element, i) => {
+            if (element.name == name){
+                setSelectedItem(element);
+            }
+        })
+    }
+    */
+
+    const CompanyCard = ({name, logo, mark}: CompanyData) => {
+        return(
+            /*<button className="company-card" onClick={() => filterList(name)}>*/
+            <button className="company-card">
+                <div className="company-card-header">
+                    <p className="company-name">{name}</p>
+                </div>
+                <div className="company-card-body">
+                    <img className="company-icon" src={logo} width="37" height="37"></img>
+                </div>
+                <div className="company-card-footer">
+                    <p>{mark}</p>
+                </div>
+            </button>
+        );
+    };
+
+    return (
+        <div className="companies-grid">
+            {companies.map((company, i) => (
+                <CompanyCard key={i} name={company.name} logo={company.logo} mark={company.mark}/>
+            ))}
+        </div>
+    );
+};
+
+export default CompanyList;
